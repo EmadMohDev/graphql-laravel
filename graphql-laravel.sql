c5f49f36e06cfd25a6aca9d0815d3a4c8b599a00
emad.mohamed.ivas <emad@ivas.com.eg>
2022-10-10 17:13:18 +0200

    graphQl add Department mutations

--- graphql-laravel.sql
+++ graphql-laravel.sql
@@ -10,10 +10,35 @@ Target Server Type    : MYSQL
 Target Server Version : 50733
 File Encoding         : 65001
 
-Date: 2022-10-09 17:03:05
+Date: 2022-10-10 17:12:49
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `departments`
+-- ----------------------------
+DROP TABLE IF EXISTS `departments`;
+CREATE TABLE `departments` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `manager_id` bigint(20) unsigned DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `departments_title_unique` (`title`),
+  UNIQUE KEY `departments_email_unique` (`email`),
+  KEY `departments_manager_id_foreign` (`manager_id`),
+  CONSTRAINT `departments_manager_id_foreign` FOREIGN KEY (`manager_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of departments
+-- ----------------------------
+INSERT INTO `departments` VALUES ('1', 'Development', 'dev@gamil.com', '1', null, null);
+INSERT INTO `departments` VALUES ('2', 'operation22', 'operation@gmail.com', '2', null, '2022-10-10 14:01:58');
+INSERT INTO `departments` VALUES ('12', 'test', 'test@gmail.com', null, '2022-10-10 14:02:52', '2022-10-10 14:02:52');
 
 -- ----------------------------
 -- Table structure for `failed_jobs`
@@ -44,7 +69,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Records of migrations
@@ -54,6 +79,8 @@ INSERT INTO `migrations` VALUES ('2', '2014_10_12_100000_create_password_resets_
 INSERT INTO `migrations` VALUES ('3', '2019_08_19_000000_create_failed_jobs_table', '1');
 INSERT INTO `migrations` VALUES ('4', '2019_12_14_000001_create_personal_access_tokens_table', '1');
 INSERT INTO `migrations` VALUES ('5', '2022_10_09_091653_create_posts_table', '1');
+INSERT INTO `migrations` VALUES ('6', '2014_10_12_000000_create_departments_table', '2');
+INSERT INTO `migrations` VALUES ('7', '2022_10_10_104950_add_department_id_to_users', '3');
 
 -- ----------------------------
 -- Table structure for `password_resets`
@@ -132,16 +159,19 @@ CREATE TABLE `users` (
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
+  `department_id` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`)
+  UNIQUE KEY `users_email_unique` (`email`),
+  KEY `users_department_id_foreign` (`department_id`),
+  CONSTRAINT `users_department_id_foreign` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES ('1', 'Prof. Liam Klocko', 'njohnson@example.org', '2022-10-09 10:59:56', '$2y$10$eXiHnvhgDO2NgRnxGwMGROFOCLe2CnZ/1TeJAj.l7x95qhldlPxsu', 'a6Pksg7Qgp', '2022-10-09 10:59:56', '2022-10-09 10:59:56');
-INSERT INTO `users` VALUES ('2', 'Prof. Jovany Schuppe', 'howe.vena@example.org', '2022-10-09 10:59:56', '$2y$10$eXiHnvhgDO2NgRnxGwMGROFOCLe2CnZ/1TeJAj.l7x95qhldlPxsu', '2L5lTrFLTw', '2022-10-09 10:59:56', '2022-10-09 10:59:56');
-INSERT INTO `users` VALUES ('3', 'Mr. Zander Bode Jr.', 'jokeefe@example.org', '2022-10-09 10:59:56', '$2y$10$eXiHnvhgDO2NgRnxGwMGROFOCLe2CnZ/1TeJAj.l7x95qhldlPxsu', 'VFLR7SozaV', '2022-10-09 10:59:56', '2022-10-09 10:59:56');
-INSERT INTO `users` VALUES ('4', 'Cristopher O\'Hara', 'sadye14@example.net', '2022-10-09 10:59:56', '$2y$10$eXiHnvhgDO2NgRnxGwMGROFOCLe2CnZ/1TeJAj.l7x95qhldlPxsu', 'o5MeNBdo5D', '2022-10-09 10:59:56', '2022-10-09 10:59:56');
-INSERT INTO `users` VALUES ('5', 'Dr. Rhett Reichel Jr.', 'christelle76@example.com', '2022-10-09 10:59:56', '$2y$10$eXiHnvhgDO2NgRnxGwMGROFOCLe2CnZ/1TeJAj.l7x95qhldlPxsu', 'E4TXRI82qp', '2022-10-09 10:59:56', '2022-10-09 10:59:56');
-INSERT INTO `users` VALUES ('6', 'John Doe', 'Johndoe@gmail.com', null, '$2y$10$OeedtkFGMsQkcioNPGbPw.FmkIMyWV6V3MRBzD3xxUgGdRNcq28Se', null, '2022-10-09 12:49:05', '2022-10-09 12:49:05');
+INSERT INTO `users` VALUES ('1', 'Prof. Liam Klocko', 'njohnson@example.org', '2022-10-09 10:59:56', '$2y$10$eXiHnvhgDO2NgRnxGwMGROFOCLe2CnZ/1TeJAj.l7x95qhldlPxsu', 'a6Pksg7Qgp', '2022-10-09 10:59:56', '2022-10-09 10:59:56', '1');
+INSERT INTO `users` VALUES ('2', 'Prof. Jovany Schuppe', 'howe.vena@example.org', '2022-10-09 10:59:56', '$2y$10$eXiHnvhgDO2NgRnxGwMGROFOCLe2CnZ/1TeJAj.l7x95qhldlPxsu', '2L5lTrFLTw', '2022-10-09 10:59:56', '2022-10-09 10:59:56', '1');
+INSERT INTO `users` VALUES ('3', 'Mr. Zander Bode Jr.', 'jokeefe@example.org', '2022-10-09 10:59:56', '$2y$10$eXiHnvhgDO2NgRnxGwMGROFOCLe2CnZ/1TeJAj.l7x95qhldlPxsu', 'VFLR7SozaV', '2022-10-09 10:59:56', '2022-10-09 10:59:56', '2');
+INSERT INTO `users` VALUES ('4', 'Cristopher O\'Hara', 'sadye14@example.net', '2022-10-09 10:59:56', '$2y$10$eXiHnvhgDO2NgRnxGwMGROFOCLe2CnZ/1TeJAj.l7x95qhldlPxsu', 'o5MeNBdo5D', '2022-10-09 10:59:56', '2022-10-09 10:59:56', '2');
+INSERT INTO `users` VALUES ('5', 'Dr. Rhett Reichel Jr.', 'christelle76@example.com', '2022-10-09 10:59:56', '$2y$10$eXiHnvhgDO2NgRnxGwMGROFOCLe2CnZ/1TeJAj.l7x95qhldlPxsu', 'E4TXRI82qp', '2022-10-09 10:59:56', '2022-10-09 10:59:56', '2');
+INSERT INTO `users` VALUES ('6', 'John Doe', 'Johndoe@gmail.com', null, '$2y$10$OeedtkFGMsQkcioNPGbPw.FmkIMyWV6V3MRBzD3xxUgGdRNcq28Se', null, '2022-10-09 12:49:05', '2022-10-09 12:49:05', null);
